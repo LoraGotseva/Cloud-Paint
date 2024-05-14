d428710ffd35bc3beae2ad2cf52d0f83cde235e9
Aleksandar Slavov <117994598+AlexandarVS@users.noreply.github.com>
2024-05-14 18:10:14 +0300

    Update saveImage.js

--- assets/js/saveImage.js
+++ assets/js/saveImage.js
@@ -1,5 +1,4 @@
 const canvas = document.querySelector("canvas");
-const clearCanvas = document.querySelector(".clear-button");
 const saveImg = document.querySelector(".download-button");
 const ctx = canvas.getContext("2d");
 let selectedColor = "#000";
