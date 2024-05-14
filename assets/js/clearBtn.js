384710d9212004cf6d979a4d5d7f107a4730058d
Aleksandar Slavov <117994598+AlexandarVS@users.noreply.github.com>
2024-05-14 21:34:44 +0300

    Update clearBtn.js

--- assets/js/clearBtn.js
+++ assets/js/clearBtn.js
@@ -1,6 +1,8 @@
 const clearBtn = document.getElementsByClassName('clear-button')[0];
 
-clearBtn.addEventListener("click", () => {
+const erase = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     setCanvasBackground();
-});
+}
+
+clearBtn.addEventListener("click",erase)
