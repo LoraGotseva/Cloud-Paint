92ba07e3fd2abe6107a00baead5d525671c92791
LoraGotseva <lora.gotseva@abv.bg>
2024-06-27 02:10:20 +0300

    Made the code more readable

--- assets/js/functionalities/cursorChanger.js
+++ assets/js/functionalities/cursorChanger.js
@@ -1,37 +1,18 @@
 const elementIds = ["brush", "eraser", "text", "rectangle", "circle", "triangle", "line"];
 
 elementIds.forEach((id) => {
-  document.getElementById(id).addEventListener("click", () => {
-    document.body.classList.remove("brush-cursor");
-    document.body.classList.remove("eraser-cursor");
-    document.body.classList.remove("text-cursor");
+  const element = document.getElementById(id);
+  element.addEventListener("click", () => {
+    document.body.classList.remove("brush-cursor", "eraser-cursor", "text-cursor", "default-cursor");
+    
+    if (id === "brush") {
+      document.body.classList.add("brush-cursor");
+    } else if (id === "eraser") {
+      document.body.classList.add("eraser-cursor");
+    } else if (id === "text") {
+      document.body.classList.add("text-cursor");
+    } else {
+      document.body.classList.add("default-cursor");
+    }
   });
-});
-
-document.getElementById("brush").addEventListener("click", () => {
-  document.body.classList.add("brush-cursor");
-});
-
-document.getElementById("eraser").addEventListener("click", () => {
-  document.body.classList.add("eraser-cursor");
-});
-
-document.getElementById("text").addEventListener("click", () => {
-  document.body.classList.add("text-cursor");
-});
-
-document.getElementById("rectangle").addEventListener("click", () => {
-  document.body.classList.add("default-cursor");
-});
-
-document.getElementById("circle").addEventListener("click", () => {
-  document.body.classList.add("default-cursor");
-});
-
-document.getElementById("triangle").addEventListener("click", () => {
-  document.body.classList.add("default-cursor");
-});
-
-document.getElementById("line").addEventListener("click", () => {
-  document.body.classList.add("default-cursor");
 });
