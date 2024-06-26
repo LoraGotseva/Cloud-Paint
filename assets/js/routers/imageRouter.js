8e405ea6adbfd77c360a23aa26ce15618c0e64f8
A-KeranoF <91877625+A-KeranoF@users.noreply.github.com>
2024-06-26 23:34:39 +0300

    Removed routers and controllers

--- assets/js/routers/imageRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const imageController = require('../controllers/imageController')
-const router = express.Router();
-router.get('/img', imageController.findAll);
-router.get('/img/:id', imageController.findOne);
-router.post('/img', imageController.create);
-router.patch('/img/:id', imageController.update);
-router.delete('/img/:id', imageController.destroy);
-module.exports = router
