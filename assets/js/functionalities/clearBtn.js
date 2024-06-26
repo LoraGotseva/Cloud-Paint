18dcce2cf66be58ca9141f4c45194803e17362bd
Davidenchi <david_petrov@abv.bg>
2024-06-26 23:44:04 +0300

    Minor changes

--- assets/js/functionalities/clearBtn.js
+++ assets/js/functionalities/clearBtn.js
@@ -1,13 +1,6 @@
-const clearBtn = document.querySelector('.clear-button');
-
-const setCanvasBackground = () => {
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
-
 const clear = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
     setCanvasBackground();
-}
+};
 
 clearBtn.addEventListener("click", clear);
